89d0178b262921ff4e8c5f3d0f4698868e4775bb
Amena Ahmed <am.ahmed09@gmail.com>
2018-11-19 14:53:28 -0600

    Added Zomato API to do basic restaurant search; added display divs to HTML

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,24 +9,41 @@
   };
   firebase.initializeApp(config);
 
+//Zomato API to find restaurant 
+var apiKey = "7a92ebe9a7f0e1c5487a3ea08e3ef1e2";
 
-// var queryURL = "https://api.yelp.com/v3/";
-// var apiKey = "8Em9E2JthZrHTdWxKOk08-mIEc-wA3P2WfNONM3pj8OXMkQmByxinWG3KwgbfeoTrfwuJ0HeTvIL27_wMdnNSppsW_i9Q2bMjU_CyvEe5VK8i9PD8qlEUwx51kLuW3Yx"
-//
-//
-//
-// $.ajax({
-//   url: queryURL,
-//   method: "GET",
-//   headers: {
-//     "accept": "application/json",
-//     "Access-Control-Allow-Origin":"*",
-//     "Authorization": `Bearer ${apiKey}`
-// }
-// }).then(function(res) {
-// var results = res.data
-// console.log(results);
-// })
+var rapid = new RapidAPI("default-application_5bd7a6a6e4b0a5d5a03b6ba4", "388648e2-e45b-4a7d-a751-a2e38784ef00");
+
+//variable to hold what kind of restaurant user is looking for
+var cuisine = "";
+
+
+//API call will get restaurants of type var cuisine in "searc query" near "coordinates" pre-set below
+$("#cuisine-find-btn").on("click", function(){
+cuisine = $("#cuisine-input").val().trim();
+
+rapid.call('Zomato', 'search', { 
+	'apiKey': '7a92ebe9a7f0e1c5487a3ea08e3ef1e2',
+	'coordinates': '42.032402, -87.741623',
+	'entityType': 'city',
+	'count': '1',
+	'radiusSearch': '10000',
+	'entityId': '292',
+	'searchQuery': cuisine,
+	'offset': '0',
+	// 'sort': 'realDistance'
+
+}).on('success', function (payload) {
+    console.log(payload); 
+    $(".restaurant-name").html(payload.result.restaurants[0].restaurant.name);
+    $(".restaurant-location").html(payload.result.restaurants[0].restaurant.location.address);
+    $(".restaurant-rating").html(payload.result.restaurants[0].restaurant.user_rating.aggregate_rating);
+}).on('error', function (payload) {
+	 /*YOUR CODE GOES HERE*/ 
+});
+});
+
+// We may possible the below code to run another API call.
 $("#submit-btn").on("click", function(){
 var userInput = $("#user-input").val().trim();
 console.log(userInput)
